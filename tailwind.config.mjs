e7e59ed93a6f1fff1ad1370edd9dcd783fb5d9b9
Manuel Mestres <mmestres-onestack@gmail.com>
2023-12-30 15:40:20 +0100

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,64 @@
+import animated from 'tailwindcss-animated'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				primary: '#df54b7',
+				secondary: '#7ad8a6',
+				principal: '#FFFBFB',
+				background: '#13020E'
+			},
+			keyframes: {
+				pulsesecondary: {
+					'0%': {
+						color: '#7ad8a6',
+						borderColor: '#7ad8a6',
+						transform: 'scale(0.95)',
+						boxShadow: '0 0 5px 0 #7ad8a6',
+					},
+					'70%': {
+						color: '#7ad8a6',
+						borderColor: '#7ad8a6',
+						transform: 'scale(1.05)',
+						boxShadow: '0 0 5px 10px transparent',
+					},
+					'100%': {
+						color: '#7ad8a6',
+						borderColor: '#7ad8a6',
+						transform: 'scale(1)',
+						boxShadow: '0 0 10px 0 transparent',
+					},
+				},
+				pulseprimary: {
+					'0%': {
+						color: '#df54b7',
+						borderColor: '#df54b7',
+						transform: 'scale(0.95)',
+						boxShadow: '0 0 5px 0 #df54b7',
+					},
+					'70%': {
+						color: '#df54b7',
+						borderColor: '#df54b7',
+						transform: 'scale(1.05)',
+						boxShadow: '0 0 5px 10px transparent',
+					},
+					'100%': {
+						color: '#df54b7',
+						borderColor: '#df54b7',
+						transform: 'scale(1.05)',
+						boxShadow: '0 0 10px 0 transparent',
+					},
+				}
+			},
+			animation: {
+				pulseprimary: 'pulseprimary 1s ease-out forwards',
+				pulsesecondary: 'pulsesecondary 1s ease-out forwards',
+			},
+		},
+	},
+	plugins: [animated],
+}
